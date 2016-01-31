8c77188b95b82a2a89e30e138dbb54b101222526
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-01-31 15:16:07 +0200

    average rating for brewery

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,12 @@
 class Brewery < ActiveRecord::Base
-  has_many :beers, dependent: destroy
+  has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+
+  def average_rating
+    avg = 0.0
+    self.ratings.each do |rating|
+      avg += rating.score
+    end
+    avg / self.ratings.size
+  end
 end
