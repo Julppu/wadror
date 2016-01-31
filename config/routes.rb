612b959020fc1cdcc6d5acd2ae5ded8cb2114fff
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-01-31 22:23:09 +0200

    create sessions

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
 
   get 'signup', to: 'users#new'
+  
+  resource :session, only: [:new, :create, :delete]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
