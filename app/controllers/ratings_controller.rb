26380941871d2c58c5fef0bc1c10a12d210b2c8e
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-02-03 19:59:05 +0200

    apply validation

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,10 +5,14 @@ class RatingsController < ApplicationController
   end
 
   def create
-    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-    # save a completed rating to session
-    current_user.ratings << rating
-    redirect_to current_user
+    @rating = Rating.new params.require(:rating).permit(:score, :beer_id)
+    if @rating.save
+      current_user.ratings << @rating
+      redirect_to user_path current_user
+    else
+      @beers = Beer.all
+      render :new
+    end
   end
 
   def new
