96b5744d881edfed818f33d74ff5ef4411591767
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-02-03 20:51:57 +0200

    slightly different validation

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,7 +3,7 @@ class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
-  validates :year, numericality: { greater_than_or_equal_to: 1024,
-                                   less_than_or_equal_to: Time.now.year }
+  validates :year, numericality: { greater_than_or_equal_to: 1042,
+                                   less_than_or_equal_to: Date.current.in_time_zone.year }
   validates :name, presence: true
 end
