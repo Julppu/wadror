a39f6517abc6290e8fb908c035f21a5776b1d958
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-02-03 19:58:51 +0200

    validation for models

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,4 +3,7 @@ class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
+  validates :year, numericality: { greater_than_or_equal_to: 1024,
+                                   less_than_or_equal_to: Time.now.year }
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   include RatingsAverage
   has_many :ratings
+
+  validates :username, uniqueness: true,
+            length: { minimum: 3 },
+            length: { maximum: 15 }
+
 end
