67074d34e466d155bfb561ff1014b140cbabf1e9
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-02-07 18:26:04 +0200

    add password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
 
+  has_secure_password
+
   validates :username, uniqueness: true,
             length: { minimum: 3 },
             length: { maximum: 15 }
