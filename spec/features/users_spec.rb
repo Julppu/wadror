98651a16c636157e4a4003bb667365d9e37b0faa
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-02-11 22:10:28 +0200

    refactor tests to use helper module

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
+include Helpers
+
 describe "User" do
   before :each do
     FactoryGirl.create :user
+  end
+
+  describe "who has signed up" do
+    it "can signin with right credentials" do
+      sign_in(username:"Pekka", password:"Foobar1")
+
+      expect(page).to have_content 'Welcome back!'
+      expect(page).to have_content 'Pekka'
+    end
+
+    it "is redirected back to signin form if wrong credentials given" do
+      sign_in(username:"Pekka", password:"wrong")
+
+      expect(current_path).to eq(signin_path)
+      expect(page).to have_content 'username and password do not match'
+    end
   end
 
   it "when signed up with good credentials, is added to the system" do
@@ -14,27 +32,5 @@ describe "User" do
     expect{
       click_button('Create User')
     }.to change{User.count}.by(1)
-  end
-
-  describe "who has signed up" do
-    it "can signin with right credentials" do
-      visit signin_path
-      fill_in('username', with:'Pekka')
-      fill_in('password', with:'Foobar1')
-      click_button('Log in')
-
-      expect(page).to have_content 'Welcome back!'
-      expect(page).to have_content 'Pekka'
-    end
-
-    it "is redirected back to signin form if wrong credentials given" do
-      visit signin_path
-      fill_in('username', with:'Pekka')
-      fill_in('password', with:'wrong')
-      click_button('Log in')
-
-      expect(current_path).to eq(signin_path)
-      expect(page).to have_content 'Username and/or password mismatch'
-    end
   end
 end
