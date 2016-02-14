bce268e2e51c521ad11446742e194ba45459a9b8
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-02-14 20:50:27 +0200

    show ratings correctly

---
+++ spec/features/users_page_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+include Helpers
+
+describe "User" do
+  let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
+  let!(:beer1) { FactoryGirl.create :beer, name:"iso 3", brewery:brewery }
+  let!(:beer2) { FactoryGirl.create :beer, name:"Karhu", brewery:brewery }
+  let!(:beer3) { FactoryGirl.create :beer, name:"Sandels", brewery:brewery }
+  let!(:user) { FactoryGirl.create :user }
+
+  before :each do
+    sign_in(username:"Pekka", password:"Foobar1")
+  end
+
+  it "shows ratings correctly" do
+    rating_score = { 5 => "iso 3", 15 => "Karhu", 20 => "Sandels"  }
+    rating_score.each do |score, name|
+      visit new_rating_path
+      select(name, from:'rating[beer_id]')
+      fill_in('rating[score]', with:score)
+      click_button "Create Rating"
+    end
+
+    visit user_path user
+    expect(page).to have_content "Has made 3 ratings"
+    expect(page).to have_content "Sandels: 20"
+  end
+end
