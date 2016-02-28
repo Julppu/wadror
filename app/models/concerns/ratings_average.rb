897d9af1ca81c3dda59c9ce4115df76f968ff37b
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-02-28 22:59:33 +0200

    helper method to round decimals

--- app/models/concerns/ratings_average.rb
+++ app/models/concerns/ratings_average.rb
@@ -6,6 +6,11 @@ module RatingsAverage
     self.ratings.each do |rating|
       avg += rating.score
     end
-    avg / self.ratings.size
+    round(avg / self.ratings.size)
+  end
+
+  # helper method to round numbers to max 1 decimal
+  def round param
+    number_with_precision param, precision: 1, strip_insignificant_zeros: true
   end
 end
