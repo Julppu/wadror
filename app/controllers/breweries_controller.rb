dfb1d9df5dede5184c5e787d4c05bae05518f874
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-02-28 19:52:53 +0200

    separate active and retired breweries

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -7,12 +7,19 @@ class BreweriesController < ApplicationController
   # GET /breweries
   # GET /breweries.json
   def index
-    @breweries = Brewery.all
+    @active_breweries = Brewery.where(active:true)
+    @retired_breweries = Brewery.where(active:[nil, false])
     order = params[:order] || 'name'
-    @beers = case order
-               when 'name' then @breweries.sort_by{ |b| b.name }
-               when 'year' then @breweries.sort_by{ |b| b.year }
-             end
+
+    @active_breweries = case order
+               when 'name' then @active_breweries.sort_by{ |b| b.name }
+               when 'year' then @active_breweries.sort_by{ |b| b.year }
+                        end
+
+    @retired_breweries = case order
+                          when 'name' then @retired_breweries.sort_by{ |b| b.name }
+                          when 'year' then @retired_breweries.sort_by{ |b| b.year }
+                        end
   end
 
   # GET /breweries/1
@@ -77,7 +84,7 @@ class BreweriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def brewery_params
-      params.require(:brewery).permit(:name, :year)
+      params.require(:brewery).permit(:name, :year, :active)
     end
 
     def authenticate
