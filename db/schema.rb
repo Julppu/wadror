1fce7cc2bd428d99c4761a582587a7a088c81a23
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-02-28 23:07:05 +0200

    add admin field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228174334) do
+ActiveRecord::Schema.define(version: 20160228210534) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160228174334) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
 end
